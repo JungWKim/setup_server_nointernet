53d7609fc347e2c8b573dac28e7c564a0c627987
Jeong Woo Kim <50034678+JungWKim@users.noreply.github.com>
2022-10-27 00:05:15 +0900

    Update download_packages_centos7.9.sh

--- download_packages_centos7.9.sh
+++ download_packages_centos7.9.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-yum install -y epel-release --downloadonly --downloaddir=centos7.9/gpu/
+yum install -y epel-release --downloadonly --downloaddir=centos7/gpu/
 
 yum install -y epel-release
 
-yum install -y net-tools createrepo pciutils --downloadonly --downloaddir=centos7.9/basic/
+yum install -y net-tools createrepo pciutils --downloadonly --downloaddir=centos7/basic/
 
-yum update -y --downloadonly --downloaddir=centos7.9/update/
+yum update -y --downloadonly --downloaddir=centos7/update/
 
-yum install -y kernel-devel --downloadonly --downloaddir=centos7.9/gpu/
+yum install -y kernel-devel --downloadonly --downloaddir=centos7/gpu/
 
-yum install -y dkms --downloadonly --downloaddir=centos7.9/gpu/
+yum install -y dkms --downloadonly --downloaddir=centos7/gpu/
 
-yum install -y yum-utils --downloadonly --downloaddir=centos7.9/docker/
+yum install -y yum-utils --downloadonly --downloaddir=centos7/docker/
 
 yum install -y yum-utils
 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin --downloadonly --downloaddir=centos7.9/docker/
+yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin --downloadonly --downloaddir=centos7/docker/
 
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 
@@ -26,4 +26,4 @@ curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.re
 
 yum clean expire-cache
 
-yum install -y nvidia-container-toolkit --downloadonly --downloaddir=centos7.9/nvidia-container-toolkit/
+yum install -y nvidia-container-toolkit --downloadonly --downloaddir=centos7/nvidia-container-toolkit/
