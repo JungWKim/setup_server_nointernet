4424188b286547c83f875a5b0b76598b677bc63f
JungWKim <lionmini@naver.com>
2022-10-29 16:21:37 +0900

    .

--- download_packages_ubuntu22.04.sh
+++ download_packages_ubuntu22.04.sh
@@ -39,10 +39,10 @@ func_check_prerequisite() {
 		# check OS distribution
 		local OS_DIST=$(. /etc/os-release;echo $ID$VERSION_ID)
 
-		if [ "${OS_DIST}" == "ubuntu20.04" ] ; then
-			logger -s "[INFO] OS distribution matches ubuntu20.04"
+		if [ "${OS_DIST}" == "ubuntu22.04" ] ; then
+			logger -s "[INFO] OS distribution matches ubuntu22.04"
 		else
-			logger -s "[Error] OS distribution doesn't match ubuntu20.04"
+			logger -s "[Error] OS distribution doesn't match ubuntu22.04"
 			exit 1
 		fi
 	fi
