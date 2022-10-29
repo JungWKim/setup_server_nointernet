c9c346b0b8c475113f128d9ec217cc375c1f3830
JungWKim <lionmini@naver.com>
2022-10-29 13:27:46 +0900

    .

--- download_packages_centos7.9.sh
+++ download_packages_centos7.9.sh
@@ -1,29 +1,107 @@
 #!/bin/bash
 
-yum install -y epel-release --downloadonly --downloaddir=centos7/gpu/
+#--------- variables
+#--------- change to "yes" only
+SAVE_DIR=
+BASIC=no
+UPDATE=no
+GPU_RELATED=no
+DOCKER=no
+NVIDIA_CONTAINER_TOOLKIT=no
 
-yum install -y epel-release
+#--------- function whether variables are deinfed
+func_check_variable() {
 
-yum install -y net-tools createrepo pciutils --downloadonly --downloaddir=centos7/basic/
+	local ERROR_PRESENCE=0
 
-yum update -y --downloadonly --downloaddir=centos7/update/
+	if [ -z ${SAVE_DIR} ] ; then
+		logger -s "[Error] SAVE_DIR is not defined." ; ERROR_PRESENCE=1 ; fi
+	if [ -z ${BASIC} ] ; then
+		logger -s "[Error] BASIC is not defined." ; ERROR_PRESENCE=1 ; fi
+	if [ -z ${UPDATE} ] ; then
+		logger -s "[Error] UPDATE is not defined." ; ERROR_PRESENCE=1 ; fi
+	if [ -z ${GPU_RELATED} ] ; then
+		logger -s "[Error] GPU_RELATED is not defined." ; ERROR_PRESENCE=1 ; fi
+	if [ -z ${DOCKER} ] ; then
+		logger -s "[Error] DOCKER is not defined." ; ERROR_PRESENCE=1 ; fi
+	if [ -z ${NVIDIA_CONTAINER_TOOLKIT} ] ; then
+		logger -s "[Error] NVIDIA_CONTAINER_TOOLKIT is not defined." ; ERROR_PRESENCE=1 ; fi
 
-yum install -y kernel-devel --downloadonly --downloaddir=centos7/gpu/
+	if [ ${ERROR_PRESENCE} -eq 1 ] ; then
+		exit 1
+	fi	
+}
 
-yum install -y dkms --downloadonly --downloaddir=centos7/gpu/
+func_check_prerequisite() {
 
-yum install -y yum-utils --downloadonly --downloaddir=centos7/docker/
+	# /etc/os-release file existence check
+	if [ ! -e "/etc/os-release" ] ; then
+		logger -s "[Error] /etc/os-release doesn't exist. OS is unrecognizable."
+		exit 1
+	else
+		# check OS distribution
+		local OS_DIST=$(. /etc/os-release;echo $ID$VERSION_ID)
 
-yum install -y yum-utils
+		if [ "${OS_DIST}" == "centos7" ] ; then
+			logger -s "OS distribution matches centos7"
+		else
+			logger -s "OS distribution doesn't matche centos7"
+			exit 1
+		fi
+	fi
 
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	# Internet connection check
+	ping -c 5 8.8.8.8 > /dev/null
+	if [ $? -ne "0" ] ; then
+		logger -s "[Error] Network in unreachable."
+		exit 1
+	else
+		logger -s "[INFO] Network connection completed"
+	fi
 
-yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin --downloadonly --downloaddir=centos7/docker/
+	# check that directory for saving packages exists
+	ls ${SAVE_DIR}
+	if [ $? -ne 0 ] ; then
+		logger -s "[Error] ${SAVE_DIR} doesn't exist"
+		exit 1
+	fi
+}
 
-distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+#--------- call checking functions
+func_check_variable
+func_check_prerequisite
 
-curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+#----------- download basic packages
+if [ ${BASIC} == "yes" ] ; then
+	yum install -y epel-release --downloadonly --downloaddir=${SAVE_DIR}/gpu/
+	yum install -y epel-release
+	yum install -y net-tools createrepo pciutils --downloadonly --downloaddir=${SAVE_DIR}/basic/
+fi
 
-yum clean expire-cache
+#----------- download updated packages
+if [ ${UPDATE} == "yes" ] ; then
+	yum update -y --downloadonly --downloaddir=${SAVE_DIR}/update/
+fi
 
-yum install -y nvidia-container-toolkit --downloadonly --downloaddir=centos7/nvidia-container-toolkit/
+#----------- download updated packages
+if [ ${GPU_RELATED} == "yes" ] ; then
+	yum groupinstall -y "Development Tools" --downloadonly --downloaddir=${SAVE_DIR}/gpu/
+	yum install -y kernel-devel --downloadonly --downloaddir=${SAVE_DIR}/gpu/
+	yum install -y dkms --downloadonly --downloaddir=${SAVE_DIR}/gpu/
+fi
+
+#----------- download docker packages
+if [ ${DOCKER} == "yes" ] ; then
+	yum install -y yum-utils --downloadonly --downloaddir=${SAVE_DIR}/docker/
+	yum install -y yum-utils
+	yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+	yum install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin --downloadonly --downloaddir=${SAVE_DIR}/docker/
+fi
+
+#----------- download nvidia-container-toolkit packages
+if [ ${NVIDIA_CONTAINER_TOOLKIT} == "yes" ] ; then
+	distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+	curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+	yum clean expire-cache
+	yum install -y nvidia-container-toolkit --downloadonly --downloaddir=${SAVE_DIR}/nvidia-container-toolkit/
+fi
